502a6ea386deb7919f5a801a20b91967bf1175d8
Deekshith <deekshithpatel10@gmail.com>
2025-07-05 17:17:50 +0530

    feat: add +,*,-,** operations to the Tensor class, along with backward method
    Defined dunder methods for these operations. Also defined how to backpropagate gradients to parents if these functions are used. Created the backward() method which uses topo-sort

--- tensor.py
+++ tensor.py
@@ -1,70 +1,103 @@
 import numpy as np 
 
 class Tensor: 
-   def __init__(self, data, requires_grad=False, _children=(), _op=''):
+   def __init__(self, data, requires_grad=False, _parents=(), _op=''): 
       self.data = np.array(data, dtype=np.float64)
-      self.requires_grad = requires_grad
-      self.grad = 0.0
+      self.requires_grad = requires_grad 
+      self.grad = None 
 
-      # graph related attributes 
-      self._backward = lambda : None 
-      self._prev = set(_children)
-      self._op = _op
+      self._backward = lambda : None      # gradient function
+      self._prev = set(_parents)         # tensors used to create this one
+      self._op = _op                      # operation involved
 
    def __repr__(self):
-      return f"Tensor(data={self.data}, grad={self.grad})"
+      return f'Tensor(data={self.data}, grad={self.grad}, requires_grad={self.requires_grad})'
 
    def __add__(self, other): 
       other = other if isinstance(other, Tensor) else Tensor(other)
-      out = Tensor(self.data + other.data, True, (self, other), '+')
+      out = Tensor(self.data+other.data, requires_grad=(self.requires_grad or other.requires_grad), _parents=(self, other), _op='+')
 
-      def _backward(): 
-         self.grad += out.grad
-         other.grad += out.grad 
+      def _backward():
+         if self.requires_grad:
+            self.grad = self.grad + out.grad if self.grad is not None else out.grad
+         if other.requires_grad:
+            other.grad = other.grad + out.grad if other.grad is not None else out.grad
+
       out._backward = _backward
-
       return out 
-   
+
    def __mul__(self, other): 
       other = other if isinstance(other, Tensor) else Tensor(other)
-      out = Tensor(self.data * other.data, True, (self, other), '*')
+      out = Tensor(self.data+other.data, requires_grad=(self.requires_grad or other.requires_grad), _parents=(self, other), _op='*')
 
       def _backward(): 
-         self.grad += other.data * out.grad 
-         other.grad += self.data * out.grad 
+         if self.requires_grad:
+            grad_self = other.data * out.grad
+            self.grad = self.grad + grad_self if self.grad is not None else grad_self
+         if other.requires_grad:
+            grad_other = self.data * out.grad
+            other.grad = other.grad + grad_other if other.grad is not None else grad_other
+      
       out._backward = _backward
 
       return out 
-   
+
+   def __neg__(self): return self * -1
+
+   def __sub__(self, other): return self + (-other)
+
+   def __pow__(self, power):
+      assert isinstance(power, (int, float))
+      out = Tensor(self.data ** power, requires_grad=self.requires_grad, _parents=(self,), _op=f'**{power}')
+
+      def _backward():
+         if self.requires_grad:
+            grad = (power * self.data ** (power - 1)) * out.grad
+            self.grad = self.grad + grad if self.grad is not None else grad
+
+      out._backward = _backward
+      return out
+
+
    def backward(self): 
-      if not self.requires_grad:
-        raise RuntimeError("Cannot call backward on a tensor that does not require gradients.")
-      
-      # topo sort 
+      if self.grad is None: 
+         self.grad = np.ones_like(self.data)
+
       topo = []
       visited = set()
-
-      def build_topo(v): 
-         if v not in visited: 
-            visited.add(v)
-            for child in v._prev: 
-               build_topo(child)
-            topo.append(v)
-
+      def build_topo(tensor):
+         if tensor not in visited: 
+            visited.add(tensor)
+            for parent in tensor._prev: 
+               build_topo(parent)
+            topo.append(tensor)
+      
       build_topo(self)
+   
+      for tensor in reversed(topo): 
+         tensor._backward()
 
-      self.grad = 1.0   # ∂output/∂output 
-      for node in reversed(topo): 
-         node._backward()
+   def zero_grad(self): 
+      if self.requires_grad: 
+         self.grad = 0.0
+   
+   @property 
+   def shape(self): return self.data.shape 
 
-if __name__ == "__main__":
-   a = Tensor(2.0)
-   b = Tensor(3.0)
-   c = a * b + b + 1
+   #right hand methods 
+   def __radd__(self, other): return self + other
+   def __rmul__(self, other): return self * other 
+   def __rsub__(self, other): return (-self) + other 
+
+if __name__=="__main__": 
+   a = Tensor(4, requires_grad=True)
+   b = Tensor(6, requires_grad=True)
+   c =  a**2 + b
+   print(c)
    c.backward()
-
-   print("a:", a)  # Should show grad = 3.0
-   print("b:", b)  # Should show grad = 3.0 (from mult) + 1.0 (from add)
-   print("c:", c)  # Grad = 1.0 (seed)
+   print(a)
+   print(b)
 
 
+
+      
